13f606cea29c5e07da28d56140ca16019dbaaa4d
xionglei1614 <xionglei1614@163.com>
2020-12-15 11:44:55 +0800

    fix: 添加今日练习

--- src/xyz/xionglei/year2020/december/day1215/Day1215.java
+++ src/xyz/xionglei/year2020/december/day1215/Day1215.java
@@ -2,4 +2,8 @@ package xyz.xionglei.year2020.december.day1215;
 
 public class Day1215 {
 
+    public static void main(String[] args) {
+        int N = 22211;
+        System.out.println(new Solution().monotoneIncreasingDigits(N));
+    }
 }
--- src/xyz/xionglei/year2020/december/day1215/Solution.java
+++ src/xyz/xionglei/year2020/december/day1215/Solution.java
@@ -1,4 +1,43 @@
 package xyz.xionglei.year2020.december.day1215;
 
+/**
+ * leetcode:738 单调递增的数字
+ * 给定一个非负整数 N，找出小于或等于 N 的最大的整数，同时这个整数需要满足其各个位数上的数字是单调递增。
+ * （当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。）
+ * 示例 1:
+ * 输入: N = 10
+ * 输出: 9
+ * 示例 2:
+ * 输入: N = 1234
+ * 输出: 1234
+ * 示例 3:
+ * 输入: N = 332
+ * 输出: 299
+ * 说明: N 是在 [0, 10^9] 范围内的一个整数。
+ */
 public class Solution {
+
+    // my answer
+    public int monotoneIncreasingDigits(int N) {
+        String nStr = String.valueOf(N);
+        int index = 0; // 用于subString用
+        for (int i = 1; i < nStr.length(); i++) {
+            // 出现左 > 右 返回
+            if (nStr.charAt(i - 1) > nStr.charAt(i)) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(nStr.substring(0, index)).append(nStr.charAt(index) - '1');
+                for (int j = 0; j < nStr.length() - index - 1; j++) {
+                    sb.append(9);
+                }
+                return Integer.parseInt(sb.toString());
+            } else if (nStr.charAt(i - 1) == nStr.charAt(i)) {
+                if (nStr.charAt(index) != nStr.charAt(i)) {
+                    index = i - 1;
+                }
+            } else {
+                index = i;
+            }
+        }
+        return N;
+    }
 }
