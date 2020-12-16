dd3b4e6ac50128c1e461e7485df1dc2d4802454e
xionglei1614 <xionglei1614@163.com>
2020-12-16 11:53:17 +0800

    fix: 提交今日任务

---
+++ src/xyz/xionglei/year2020/december/day1216/Day1216.java
@@ -0,0 +1,10 @@
+package xyz.xionglei.year2020.december.day1216;
+
+public class Day1216 {
+
+    public static void main(String[] args) {
+        String pattern = "aad";
+        String str = "dog dog cat";
+        System.out.println(new Solution().wordPattern(pattern, str));
+    }
+}
