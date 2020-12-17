a979219876da48424e6375e0f10943caba8e51f4
xionglei1614 <xionglei1614@163.com>
2020-12-17 16:43:22 +0800

    fix: 调整代码

--- src/xyz/xionglei/year2020/december/day1217/Day1217.java
+++ src/xyz/xionglei/year2020/december/day1217/Day1217.java
@@ -3,6 +3,16 @@ package xyz.xionglei.year2020.december.day1217;
 public class Day1217 {
 
     public static void main(String[] args) {
+        E e1 = E.A;
+        E e2 = E.B;
+        E e3 = E.valueOf("A");
+        System.out.println(e1 == e3);
 
+        System.out.printf("%d", new Solution().maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+    }
+
+    enum E {
+        A,
+        B
     }
 }
